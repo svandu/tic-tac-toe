1ce6b0ed1a693540deb0f32e287c0f724ba6a33a
svandu <svandu08@gmail.com>
2023-04-07 23:15:55 +0530

    module update

--- modules/index.js
+++ modules/index.js
@@ -1,9 +1,24 @@
+import { tickIcon, crossIcon } from "./constants.js";
+
+let currentOperation = tickIcon;
+
 const createBoard = (boardCells, gameBoard) => {
-    boardCells.forEach((_) => {
+    boardCells.forEach((_,index) => {
         const boardSquare = document.createElement("div");
         boardSquare.classList.add("square");
+        boardSquare.dataset.id = index;
+        boardSquare.addEventListener("click", clickOperations); 
         gameBoard.append(boardSquare);
     })
+};
+
+const clickOperations = (e) => {
+    const operationImg = document.createElement("img");
+    operationImg.classList.add("square-img");
+    operationImg.setAttribute("src", currentOperation);
+    e.target.append(operationImg);
+
+    currentOperation = currentOperation === tickIcon ? crossIcon : tickIcon;
 };
 
 export default createBoard; 
